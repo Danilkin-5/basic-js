6686a7385cfff4c75eaf7fe88855c21b0e0451ce
Denis <danilkin58@gmail.com>
2020-03-20 19:16:07 +0300

    feat: feat: make venegar task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,25 +1,29 @@
 module.exports = function transform(arr) {
-    if(!Array.isArray(arr)){
+    if (!Array.isArray(arr)) {
         throw Error();
     }
     let arrResult = [];
-    for(let i = 0 ;i < arr.length;i++)
-    {   
-        switch(arr[i]) {
-            case '--discard-next': i++;
-            break;
-            case '--discard-prev': arrResult.pop();
-            break
-            case '--double-next': if (i + 1 <= arr.length - 1) {
-                arrResult.push(arr[i + 1]);
-              };
-            break
-            case '--double-prev': if (i - 1 >= 0) {
-                arrResult.push(arr[i - 1]);
-              };
-            break;
-            default: arrResult.push(arr[i]);
-        }       
+    for (let i = 0; i < arr.length; i++) {
+        switch (arr[i]) {
+            case '--discard-next':
+                i++;
+                break;
+            case '--discard-prev':
+                arrResult.pop();
+                break
+            case '--double-next':
+                if (i + 1 <= arr.length - 1) {
+                    arrResult.push(arr[i + 1]);
+                };
+                break
+            case '--double-prev':
+                if (i - 1 >= 0) {
+                    arrResult.push(arr[i - 1]);
+                };
+                break;
+            default:
+                arrResult.push(arr[i]);
+        }
     }
     return arrResult;
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,75 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(flag) {
+        this.flag = flag;
+        this.arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        this.obj_Num = {};
+
+        this.arr_EN.forEach((element, index) => {
+            this.obj_Num[element] = index;
+        });
+    }
+    encrypt(str, key) {
+        if (!(str && key)) throw new Error();
+        let newStr = this.getStrPart(str);
+
+        let strArr = newStr.join('').split('');
+        let keyArr = new Array(strArr.length).fill(key).join('').split('');
+        keyArr.length = strArr.length;
+
+        let strNum_Arr = strArr.map((element, index, arr) => {
+            return element.toUpperCase() in this.obj_Num ? this.obj_Num[element.toUpperCase()] : " ";
+        });
+        let keyNum_Arr = keyArr.map((element, index, arr) => {
+            return element.toUpperCase() in this.obj_Num ? this.obj_Num[element.toUpperCase()] : " ";
+        });
+
+        let result = [];
+
+        strNum_Arr.forEach((element, index, arr) => {
+            if (element === " ") {
+                result.push(element);
+                keyNum_Arr.splice(index, 0, null);
+            } else {
+                let number = element + keyNum_Arr[index] >= this.arr_EN.length ? (element + keyNum_Arr[index]) % this.arr_EN.length : element + keyNum_Arr[index];
+                result.push(this.arr_EN[number]);
+            }
+        });
+
+        return String(this.flag) == "false" ? (str.replace(newStr, result.join(''))).split('').reverse().join('') : str.replace(newStr, result.join(''));
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(str, key) {
+        if (!(str && key)) throw new Error();
+        let newStr = this.getStrPart(str);
+
+        let strArr = newStr.join('').split('');
+        let keyArr = new Array(strArr.length).fill(key).join('').split('');
+        keyArr.length = strArr.length;
+
+        let strNum_Arr = strArr.map((element, index, arr) => {
+            return element.toUpperCase() in this.obj_Num ? this.obj_Num[element.toUpperCase()] : " ";
+        });
+        let keyNum_Arr = keyArr.map((element, index, arr) => {
+            return element.toUpperCase() in this.obj_Num ? this.obj_Num[element.toUpperCase()] : " ";
+        });
+
+        let result = [];
+
+        strNum_Arr.forEach((element, index, arr) => {
+            if (element === " ") {
+                result.push(element);
+                keyNum_Arr.splice(index, 0, null);
+            } else {
+                let number = element - keyNum_Arr[index] <= 0 ? (element + this.arr_EN.length - keyNum_Arr[index]) % this.arr_EN.length : element - keyNum_Arr[index];
+                result.push(this.arr_EN[number]);
+            }
+        });
+
+        return String(this.flag) == "false" ? (str.replace(newStr, result.join(''))).split('').reverse().join('') : str.replace(newStr, result.join(''));
+    }
+
+    getStrPart(str) {
+        return str.match(new RegExp(/[A-Za-z\s]*/))
     }
 }
 
